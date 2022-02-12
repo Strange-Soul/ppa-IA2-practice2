bf43742533e04134275f5b448bf85763769c0f0c
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-02-12 10:25:10 +0000

    P3 Debug

---
+++ p6original.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void input_string(char *a)
+{
+  printf("Enter any String Do U Want to Reverse \n");
+  scanf("%s",a);
+}
+char str_reverse(char *a)
+{ 
+ int i,j;
+ for(i=0;i<)
+}
+void output(char *a,char *reverse)
+{
+printf("Reverse of String %s is %s \n",a,reverse);
+}
+int main()
+{
+  char x[20],z;
+  input_string(x);
+  z=str_reverse(x);
+  output(x,z);
+  return 0;
+}
---
+++ p7original.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+struct _triangle
+{
+ float base,altitude,area;
+};
+
+typedef struct _triangle Triangle;
+
+Triangle input_triangle()
+{
+ Triangle s;
+ printf("Enter the Base and Heigt of Triangle \n");
+ scanf("%f %f",&s.base,&s.altitude);
+ return s;
+}
+void find_area( Triangle  *t)
+{ 
+t->area=0.5*(t->base)*(t->altitude);
+}
+void output(Triangle t)
+{
+ printf("Area of triangle with base =%f and with Altitude=%f is %f \n",t.base,t.altitude,t.area);
+}
+int main()
+{
+  Triangle x,r;
+  x=input_triangle();
+  find_area(&x);
+  output(r);
+  return 0;
+}
