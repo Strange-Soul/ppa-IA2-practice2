7409902378535ae80d5251121075d860a5d02d20
Vignesh P <97089673+VikkiVignesh@users.noreply.github.com>
2022-02-22 14:26:51 +0000

    p6 Original

--- p6original.c
+++ p6original.c
@@ -1,14 +1,17 @@
 #include<stdio.h>
+#include<string.h>
 void input_string(char *a)
 {
   printf("Enter any String Do U Want to Reverse \n");
   scanf("%s",a);
 }
-char str_reverse(char *a)
+void str_reverse(char *a,char *rev)
 { 
- char p
-
- }
+ int length=strlen(a);
+ for(int i=0;i<length;i++)
+   {
+     rev[i]=a[(strlen-1)-i];
+   }
 }
 void output(char *a,char *reverse)
 {
@@ -16,9 +19,10 @@ printf("Reverse of String %s is %s \n",a,reverse);
 }
 int main()
 {
-  char x[20],z;
+  char x[20];
+  char y[20];
   input_string(x);
-  z=str_reverse(x);
-  output(x,z);
+  str_reverse(x,y);
+  output(x,y);
   return 0;
 }
